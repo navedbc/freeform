89e486998c01efc97e679c588a4cace3d2f994ac
Naved Khan <navedkhan@Naveds-MacBook-Air.local>
2023-07-08 17:26:04 -0400

    initial commit

---
+++ src/main/java/com/gcms/freeform/FreeformApplication.java
@@ -0,0 +1,13 @@
+package com.gcms.freeform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeformApplication.class, args);
+	}
+
+}
